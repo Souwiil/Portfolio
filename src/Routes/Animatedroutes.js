7d1bb205c9249dd19f665f798f5c842b29177a58
Souwiil <souhailbarktou1997@gmail.com>
2023-07-08 23:34:52 +0200

    scroll top

--- src/Routes/Animatedroutes.js
+++ src/Routes/Animatedroutes.js
@@ -1,5 +1,6 @@
 
-import { Route, Routes,} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import About from '../pages/About'
 import Contact from '../pages/Contact'
 import Home from '../pages/Home'
@@ -8,7 +9,16 @@ import Projects from '../pages/Projects'
 // import Portfolio from '../pages/Portfolio';
 
 
+
+
 const AnimatedRoute = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+
   return (
     <Routes >
       <Route path="/" element={<Home />} />
--- src/assets/back.js
+++ src/assets/back.js
@@ -5,6 +5,4 @@ import image4 from './Tech/php.png'
 import image5 from './Tech/mysql.png'
 import image6 from './Tech/postman.png'
 
-
-
 export default [image1, image2, image3, image4, image5, image6]
